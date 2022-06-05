d3bf4bd4384f6b2782bc6bae64a77ab34c707960
DjuroStrain <durobelacic@gmail.com>
2022-06-05 13:24:16 +0200

    Version 1.0

---
+++ src/main/java/selenium/SeleniumTestWrapper.java
@@ -0,0 +1,90 @@
+package selenium;
+
+import configurations.TestConfig;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.openqa.selenium.WebDriver;
+import selenium.driver.WebDriverConfig;
+import utils2.WebDriverProvider;
+import utils2.annotations.DisableCookies;
+import utils2.annotations.RepeatRule;
+import utils2.annotations.UserAgent;
+import utils2.annotations.browser.Browser;
+import utils2.annotations.browser.BrowserDimension;
+import utils2.annotations.browser.Browsers;
+
+import static org.junit.Assume.assumeFalse;
+import static org.junit.Assume.assumeTrue;
+
+
+public abstract class SeleniumTestWrapper {
+
+	// Config
+	protected static final TestConfig testConfig = new TestConfig();
+	private final WebDriverConfig webDriverConfig = new WebDriverConfig();
+	protected final WebDriverProvider webDriverProvider = new WebDriverProvider(this.webDriverConfig);
+
+	@Rule
+	public RepeatRule repeatRule = new RepeatRule();
+
+	protected WebDriver getDriver() {
+		return this.webDriverProvider.getDriver();
+	}
+
+	/**
+	 * test class annotations
+	 */
+	@Before
+	public void setUserAgent(){
+		UserAgent userAgent = this.getClass().getAnnotation(UserAgent.class);
+		if (userAgent != null) {
+			webDriverProvider.useUserAgent(userAgent.value());
+		}
+	}
+
+	@Before
+	public void disableCookies(){
+		DisableCookies cookies = this.getClass().getAnnotation(DisableCookies.class);
+		if (cookies != null) {
+			webDriverProvider.disableCookies(true);
+		}
+	}
+
+	@Before
+	public void browser() throws Exception {
+		Browser browser = this.getClass().getAnnotation(Browser.class);
+		if (browser != null){
+			if (browser.require().length > 0 && browser.skip().length == 0){
+				String browsers = concatinateBrowsers(browser.require());
+				assumeTrue("only execute test against " + browsers, browsers.contains(testConfig.getBrowser()));
+			}
+
+			if (browser.skip().length > 0 && browser.require().length == 0){
+				String browsers = concatinateBrowsers(browser.skip());
+				assumeFalse("skip test against " + browsers, browsers.contains(testConfig.getBrowser()));
+			}
+		}
+	}
+
+	private String concatinateBrowsers(Browsers[] browsers){
+		String concatinatedBrowsers = "";
+		for(Browsers browser : browsers) concatinatedBrowsers += browser.getValue() + " & ";
+		return concatinatedBrowsers.substring(0,concatinatedBrowsers.lastIndexOf("&"));
+	}
+
+	@Before
+	public void browserDimension(){
+		BrowserDimension browserDimension = this.getClass().getAnnotation(BrowserDimension.class);
+		if (browserDimension != null) {
+			getDriver().manage().window().setSize(browserDimension.value().dimension);
+		}
+	}
+
+	@After
+	public void closeBrowser(){
+		getDriver().quit();
+	}
+
+
+}
---
+++ src/main/java/utils2/annotations/UserAgent.java
@@ -0,0 +1,14 @@
+package utils2.annotations;
+
+import selenium.driver.UserAgents;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ ElementType.TYPE })
+public @interface UserAgent {
+    UserAgents value();
+}
